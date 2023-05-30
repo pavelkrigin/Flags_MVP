ea9dcae0b01f4b5eda45bd229fa9460fa5ca32ae
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-05-30 22:57:45 +0200

    ViewController_start_commit

--- Flags_MVC/ViewController/QuizViewController.swift
+++ Flags_MVC/ViewController/QuizViewController.swift
@@ -1,8 +1,14 @@
 //
 //  QuizViewController.swift
-//  Flags_MVC
+//  Flags_MVC- будет отвечать за отображение вопросов и обработку ответов пользователя
 //
 //  Created by Pavel Krigin on 30.05.2023.
 //
 
 import Foundation
+import UIKit
+
+class QuizViewController: UIViewController {
+    @IBOutlet weak var flagImageView: UIImageView!
+    
+}
