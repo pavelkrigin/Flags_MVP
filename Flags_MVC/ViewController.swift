f59df1d489cc007287269fee967fc418cd8a8c60
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-05-30 22:45:05 +0200

    Initial Commit

---
+++ Flags_MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flags_MVC
+//
+//  Created by Pavel Krigin on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
